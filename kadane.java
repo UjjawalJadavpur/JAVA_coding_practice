43e81ffb4a9ce4fa2746f7b5137a7cc1f77e7672
Ujjawal Jadavpur <ujjawalkjadavpur@gmail.com>
2025-08-16 17:49:36 +0530

    kadane improvisation done

--- kadane.java
+++ kadane.java
@@ -7,12 +7,31 @@ public class kadane {
 
         int sum = arr[0], maxSum = arr[0];
 
-        for (int i = 1; i < arr.length; i++) {
-            sum = Math.max(arr[i], sum + arr[i]);
-            maxSum = Math.max(maxSum, sum);
+        // for (int i = 1; i < arr.length; i++) {
+        //     sum = Math.max(arr[i], sum + arr[i]);
+        //     maxSum = Math.max(maxSum, sum);
+        // }
+        // System.out.println(maxSum);
+        
+        int start = 0, end = 0, tempStart = 0;
+
+        for (int i = 1; i < arr.length; i++) 
+        {
+            sum = sum + arr[i];
+
+            if (arr[i] > sum + arr[i]) {
+                tempStart = i;
+                sum = arr[i];
+            }
+
+            if (sum > maxSum) {
+                start = tempStart;
+                end = i;
+                maxSum = sum;
+            }
         }
 
-        System.out.println(maxSum);
+        System.out.println(start + " " + end + " " + maxSum);
 
     }
 
