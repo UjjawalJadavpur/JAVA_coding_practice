b92efdb4906d66a774966d9e8206ded9e076272d
Ujjawal Jadavpur <ujjawalkjadavpur@gmail.com>
2025-08-16 17:45:14 +0530

    kadane change done

--- kadane.java
+++ kadane.java
@@ -3,7 +3,7 @@ public class kadane {
 
     public static void main(String[] args) {
 
-        int arr[] = {3, 2, 4, -6, 8, 5, 3, -2, 7};
+        int arr[] = {3, 2, 4, -6, -8, 5, 3, -2, 7};
 
         int sum = arr[0], maxSum = arr[0];
 
